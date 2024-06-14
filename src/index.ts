71de55b579a4c6718a6f5042b64c60979a2563aa
Alyson Vilela do Nascimento <alyson.nascimento.cit@naturapay.net>
2024-06-14 01:36:52 -0300

    feat: env

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { envs } from './envs'
 
 const app = new Hono()
 
@@ -6,4 +7,7 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-export default app
+export default {
+  port: envs.port,
+  app
+}
