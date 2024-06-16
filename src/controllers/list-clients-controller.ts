a889f8ebf4c6cb6cf0761624991e9ffb7d5e90da
Alyson Vilela do Nascimento <alyson.nascimento.cit@naturapay.net>
2024-06-16 00:38:00 -0300

    fix: dto

--- src/controllers/list-clients-controller.ts
+++ src/controllers/list-clients-controller.ts
@@ -3,16 +3,19 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { ListClientsUseCase } from "../services/list-clients";
 
 const paginationDTO = z.object({
-    page: z.number().min(1),
-    pageSize: z.number().min(10),
+    page: z.number().min(1).default(1),
+    pageSize: z.number().min(10).default(10),
 })
 
 export class ListClientsController {
     constructor(private readonly listClientsUseCase: ListClientsUseCase) { }
     async handler(req: FastifyRequest, reply: FastifyReply) {
-        const query = req.query
+        const query = req.query as z.infer<typeof paginationDTO>
 
-        const { success, data: result } = paginationDTO.safeParse(query)
+        const { success, data: result } = paginationDTO.safeParse({
+            page: Number(query.page),
+            pageSize: Number(query.pageSize)
+        })
 
         if (!success) {
             return reply.code(400).send({
